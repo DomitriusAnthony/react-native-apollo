c81437ea6844fdaec1201ca1387a7e8e787b6657
DomitriusAnthony <domitriusaclark@gmail.com>
2020-02-21 00:47:09 -0500

    Added apollo for note taking data management, swapped from state.

--- src/screens/ViewNotes.js
+++ src/screens/ViewNotes.js
@@ -2,54 +2,66 @@ import React from 'react';
 import { StyleSheet, View, FlatList } from 'react-native';
 import { Text, FAB, List } from 'react-native-paper';
 import Header from '../components/Header';
+import { useQuery, useMutation, gql } from '@apollo/client';
+
+const ADD_NOTE = gql`
+  mutation AddNote($title: String! $body: String!) {
+    addNote(title: $title body: $body) @client {
+      id
+      title
+      body
+    }
+  }
+`
+const GET_NOTES = gql`
+  {
+    notes @client {
+      id
+      title
+      body
+    }
+  }
+`
 
 function ViewNotes({ navigation }) {
-  const [notes, setNotes] = React.useState([]);
+  const [addNote] = useMutation(ADD_NOTE);
+  const { data } = useQuery(GET_NOTES);
 
-  const addNote = note => {
-    note.id = notes.length + 1;
-    setNotes([
-      ...notes,
-      note
-    ])
-  }
   return (
     <>
-      <Header titleText="Simple Note Taker" />
+      <Header titleText='Simple Note Taker' />
       <View style={styles.container}>
-        <View style={styles.titleContainer}>
-          {
-            notes.length === 0
-              ? (
-                <View>
-                  <Text style={styles.title}>You do not have any notes</Text>
-                </View>
-              )
-              : (
-                <FlatList
-                  data={notes}
-                  renderItem={({ item }) => {
-                    <List.Item
-                      title={item.noteTitle}
-                      description={item.noteValue}
-                      titleStyle={styles.listTitle}
-                    />
-                  }}
-
-                  keyExtractor={item => item.id.toString()}
-                />
-              )
-          }
-
-        </View>
+        {data.notes.length === 0 ? (
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>You do not have any notes</Text>
+          </View>
+        ) : (
+            <FlatList
+              data={data.notes}
+              renderItem={({ item }) => {
+                console.log(item)
+                return (
+                  <List.Item
+                    title={item.title}
+                    description={item.body}
+                    descriptionNumberOfLines={1}
+                    titleStyle={styles.listTitle}
+                  />
+                )
+              }}
+              keyExtractor={({ id }) => id.toString()}
+            />
+          )}
         <FAB
           style={styles.fab}
           small
-          icon="plus"
-          label="Add new note"
-          onPress={() => navigation.navigate('AddNotes', {
-            addNote
-          })}
+          icon='plus'
+          label='Add new note'
+          onPress={() =>
+            navigation.navigate('AddNotes', {
+              addNote
+            })
+          }
         />
       </View>
     </>
